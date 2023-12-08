d2de100b8610e052a124ea88d2900fd60f718eb6
ludo270408 <ludovica.sette7@gmaio.com>
2023-12-08 19:26:58 +0100

    [UPTADE] Pseudocode done

---
+++ script.js
@@ -0,0 +1,7 @@
+//Genero fino a 5 numeri casuali
+//Li inserisco nell'array
+//Mostro i numeri nell'html
+//Metto il timer
+//Inserire i 5 numeri usando il promt
+//Verificare se i numeri sono uguali
+//Dire quanti e quali numeri ha indovinato l'utente
