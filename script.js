a6cb05a2f7f7a8e5daed8b97d3d25aa9ad98c472
ludo270408 <ludovica.sette7@gmaio.com>
2023-12-08 19:32:50 +0100

    [UPTADE] Numbers generated and pushed in the array

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-//Genero fino a 5 numeri casuali
-//Li inserisco nell'array
+//Genero fino a 5 numeri casuali e li inserisco nell'array
+ const array_of_numbers = [];
+ while(array_of_numbers.length<5){
+    let number=Math.floor(Math.random() * 100+1);
+    if(!array_of_numbers.includes(number)){
+        array_of_numbers.push(number);
+    }
+ }
 //Mostro i numeri nell'html
 //Metto il timer
 //Inserire i 5 numeri usando il promt
