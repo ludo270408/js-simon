84bdf4ca7a960fc15f6077c58655fe7330afa804
ludo270408 <ludovica.sette7@gmaio.com>
2023-12-08 20:52:13 +0100

    [UPTADE] Numbers sof the user

--- script.js
+++ script.js
@@ -10,7 +10,15 @@
 document.getElementById('numbers').innerText=array_of_numbers;
 //Metto il timer
 setTimeout(resetDom, 5000);
-//Inserire i 5 numeri usando il promt
-//Array dell'utente
-//Verificare se i numeri sono uguali
-//Dire quanti e quali numeri ha indovinato l'utente
+//Inserire i 5 numeri nell'array utente usando il promt dopo i 30 secondi
+setTimeout(function(){
+    const users_numbers=[];
+    while(users_numbers.length<5){
+        let number= parseInt(prompt('Inserisci il numero'));
+        if(!isNaN(number)){
+            users_numbers.push(number);
+        }
+    }
+}, 6000);
+//Verificare se i numeri sono uguali e quali e quanti numeri ha indovinato l'utente
+function CheckNumber()
